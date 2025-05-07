e48f1d7125298fe6f328dc5d9c122483b0786633
Arman Alam <armanalam78578@gmail.com>
2025-05-07 21:41:26 +0530

    trpc-integration is completed

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,8 +1,15 @@
+'use client'
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
-  
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions())
   return (
     <div>
       Home page
+      <p>is loading: {categories.isLoading}</p>
+      {JSON.stringify(categories.data)}
     </div>
   );
 }
--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -18,7 +18,7 @@ const CategoryDropdown = ({
   isActive,
   isNavigationHovered,
 }: Props) => {
-  const [ isOpen, setIsOpen ] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const { getDropdownPosition } = useDropdownPosition(dropdownRef);
@@ -53,18 +53,23 @@ const CategoryDropdown = ({
           className={cn(
             "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
             isActive && !isNavigationHovered && "bg-white border-primary",
-            isOpen && "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]",
+            isOpen &&
+              "bg-white border-primary shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[4px] -translate-y-[4px]"
           )}
         >
-          <Link prefetch href={`/${category.slug === 'all' ? '' : category.slug}`}></Link>
-          {category.name}
+          <Link
+            prefetch
+            href={`/${category.slug === "all" ? "" : category.slug}`}
+          >
+            {category.name}
+          </Link>
         </Button>
 
         {category.subcategories && category.subcategories.length > 0 && (
           <div
             className={cn(
-              'opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-l-transparent border-r-transparent border-b-[10px] border-b-black left-1/2 -translate-x-1/2',
-              isOpen && 'opacity-100'
+              "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-l-transparent border-r-transparent border-b-[10px] border-b-black left-1/2 -translate-x-1/2",
+              isOpen && "opacity-100"
             )}
           />
         )}
